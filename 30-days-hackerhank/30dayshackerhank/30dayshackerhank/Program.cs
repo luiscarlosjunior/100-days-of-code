f8ec9cee08761f4c2be0eb6030eb0fa381ab0266
Luis Santos <luis.carlosjr@hotmail.com.br>
2020-04-21 21:49:00 -0300

    Added new activites

---
+++ 30-days-hackerhank/30dayshackerhank/30dayshackerhank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30dayshackerhank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double d;
+            string s;
+
+            i = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToDouble(Console.ReadLine());
+            s = Console.ReadLine();
+
+            Console.WriteLine(i + d);
+            Console.WriteLine("{0:F1}", d + d);
+            Console.WriteLine("HackerHank " + s);
+
+        }
+
+
+
+    }
+}
