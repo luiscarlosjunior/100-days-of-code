ff3c34a516784f85196c7c30f83893188f34849c
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-06-23 17:39:34 -0300

    Added the new project

---
+++ Hackerhank/ChanllengesHanckerHank/ChanllengesHanckerHank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChanllengesHanckerHank
+{
+    class Program
+    {
+        /*
+         https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+             */
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] ar = new int[] { 10, 10, 5, 6, 3, 4, 2, 3, 3, 3 };
+            HashSet<int> vs = new HashSet<int>();
+            int pares = 0;
+
+            foreach (var item in ar)
+            {
+                if (vs.Contains(item))
+                {
+                    ++pares;
+                    vs.Remove(item);
+                } else
+                {
+                    vs.Add(item);
+                }
+            }
+
+            Console.WriteLine(pares);
+        }
+    }
+}
---
+++ Hackerhank/ChanllengesHanckerHank/ChanllengesHanckerHank/Warms-challenges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChanllengesHanckerHank
+{
+    class Warms_challenges
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <link>
+        /// https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+        /// <param name="ar"></param>
+        /// <returns></returns>
+        public static int sockMerchant(int[] ar)
+        {
+            // int[] ar = new int[] { 10, 10, 5, 6, 3, 4, 2, 3, 3, 3 };
+            HashSet<int> vs = new HashSet<int>();
+            int pares = 0;
+
+            foreach (var item in ar)
+            {
+                if (vs.Contains(item))
+                {
+                    ++pares;
+                    vs.Remove(item);
+                }
+                else
+                {
+                    vs.Add(item);
+                }
+            }
+            return pares;
+        }
+    }
+}
