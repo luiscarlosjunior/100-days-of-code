630010c68cdecaae1e3cf0a0e7ad58e9da7eac7b
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-10-18 16:00:49 -0300

    Added new project about S of SOLID

---
+++ DesignerPattern C sharp/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("Eu acordei hoje");
+            j.AddEntry("Eu tomei café da manhã");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"c:\";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+        }
+    }
+}
